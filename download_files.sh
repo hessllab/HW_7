f1dc3106a82f1ce661daa824b6404fa526b09680
NamikoMachida <nm0076@mix.wvu.edu>
2019-10-02 21:57:02 -0400

    Expanded target data period as early as 2019-06-05. If I include any data earlier than this date, an error occurs.

--- download_files.sh
+++ download_files.sh
@@ -1,5 +1,5 @@
 # download_files.sh  
-# Download all historical stream flow data (temporariry from 2019-09-23 to 2019-10-02) from USGS NWIS website for the following four sites in NC.  
+# Download all historical stream flow data (temporariry from 2019-06-05 to 2019-10-02) from USGS NWIS website for the following four sites in NC.  
 # [target sites]
 # 02109500	 WACCAMAW RIVER AT FREELAND, NC  
 # 02134500	 LUMBER RIVER AT BOARDMAN, NC  
@@ -12,6 +12,6 @@
 for sitenumber in "02109500" "02134500" "02091814" "02105769"
 do
   echo "$sitenumber"
-  wget -q -O ./"$sitenumber".txt -e robots=off -np -nd "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&cb_00065=on&format=rdb&site_no="$sitenumber"&period=&begin_date=2019-09-23&end_date=2019-10-02"
+  wget -q -O ./"$sitenumber".txt -e robots=off -np -nd "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&cb_00065=on&format=rdb&site_no="$sitenumber"&period=&begin_date=2019-06-05&end_date=2019-10-02"
   mv ./"$sitenumber".txt ./"$(grep -E -m 1 -w "$sitenumber" "$sitenumber".txt | cut -d\  -f 6- | sed 's/\ /_/g')".txt
 done
