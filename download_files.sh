7b03023dfb084b5677074d358220373273f5c20e
NamikoMachida <nm0076@mix.wvu.edu>
2019-10-04 13:37:03 -0400

    Expanded data period as back as 2010-01-01. `-q` flag was deleted so that you can see the downloading process. Also, I added an argument to specify the end date of the data period. When you run this code, type in today's date as an argument in yyyy-mm-dd format so that you can include the latest stream data. (Please be patient in downloading and opening the files. Since the data are large, it will take some time to process them.)

--- download_files.sh
+++ download_files.sh
@@ -1,17 +1,17 @@
 # download_files.sh  
-# Download all historical stream flow data (temporariry from 2019-06-05 to 2019-10-02) from USGS NWIS website for the following four sites in NC.  
+# Download all the stream flow data after 2010-01-01 from USGS NWIS website for the following 4 sites in NC.  
 # [target sites]
 # 02109500	 WACCAMAW RIVER AT FREELAND, NC  
 # 02134500	 LUMBER RIVER AT BOARDMAN, NC  
 # 02091814	 NEUSE RIVER NEAR FORT BARNWELL, NC   		 
 # 02105769	 CAPE FEAR R AT LOCK #1 NR KELLY, NC  
 
-# Usage: bash download_files.sh  
+# Usage: bash download_files.sh Today's_Date(yyyy-mm-dd) 
 # Product: four text files with site numbers and site names in their filenames.  
 
 for sitenumber in "02109500" "02134500" "02091814" "02105769"
 do
   echo "$sitenumber"
-  wget -q -O ./"$sitenumber".txt -e robots=off -np -nd "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&cb_00065=on&format=rdb&site_no="$sitenumber"&period=&begin_date=2019-06-05&end_date=2019-10-02"
+  wget -O ./"$sitenumber".txt -e robots=off -np -nd "https://nwis.waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&format=rdb&site_no="$sitenumber"&period=&begin_date=2010-01-01&end_date="$1""
   mv ./"$sitenumber".txt ./"$(grep -E -m 1 -w "$sitenumber" "$sitenumber".txt | cut -d\  -f 6- | sed 's/\ /_/g')".txt
 done
