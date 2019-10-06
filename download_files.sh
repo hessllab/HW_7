2eac1ddc04609044459c45ae1b119540e0ffdcfa
NamikoMachida <nm0076@mix.wvu.edu>
2019-10-06 17:58:20 -0400

    Added `> latest_data_"$1".txt` at the end of your command line. I think we need to make a separate text file to report the current streamflow data.

--- download_files.sh
+++ download_files.sh
@@ -20,4 +20,4 @@ done
 # Usage: select the most recent data from each of the stream loactions
 # Product: site ID, most recent time of aquisition, and stream flow info for the four site locations.
 
-tail -n -1 "02091814_NEUSE_RIVER_NEAR_FORT_BARNWELL,_NC.txt" "02105769_CAPE_FEAR_R_AT_LOCK_#1_NR_KELLY,_NC.txt" "02109500_WACCAMAW_RIVER_AT_FREELAND,_NC.txt" "02134500_LUMBER_RIVER_AT_BOARDMAN,_NC.txt"
+tail -n -1 "02091814_NEUSE_RIVER_NEAR_FORT_BARNWELL,_NC.txt" "02105769_CAPE_FEAR_R_AT_LOCK_#1_NR_KELLY,_NC.txt" "02109500_WACCAMAW_RIVER_AT_FREELAND,_NC.txt" "02134500_LUMBER_RIVER_AT_BOARDMAN,_NC.txt" > latest_data_"$1".txt
