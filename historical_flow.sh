2b8136c0c24a0f34579addec0eab0dac5b171b0f
SSchreder <sis0004@mix.wvu.edu>
2019-10-07 09:49:54 -0400

    add wget

--- historical_flow.sh
+++ historical_flow.sh
@@ -1,4 +1,4 @@
 for site in "02109500" "02134500" "02091814" "02105769"
 do
-  echo "https://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=${site}&referred_module=sw&period=&begin_date=2010-01-01&end_date=2019-10-06"
+  wget -O "$site".txt "https://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=${site}&referred_module=sw&period=&begin_date=2010-01-01&end_date=2019-10-06"
 done
