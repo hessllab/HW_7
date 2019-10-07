4141f121b10b2d34a9c890e1edd3a0001c41fbe0
SSchreder <sis0004@mix.wvu.edu>
2019-10-07 10:00:27 -0400

    Description of script

--- historical_flow.sh
+++ historical_flow.sh
@@ -1,4 +1,7 @@
+# Objective: obtain USGS streamflow data for four locations in NC using wget
+# Usage: bash historical_flow.sh 
+
 for site in "02109500" "02134500" "02091814" "02105769"
 do
-  wget -O "$site".txt "https://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=${site}&referred_module=sw&period=&begin_date=2010-01-01&end_date=2019-10-06"
+  wget -O "$site".txt "https://nwis.waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=${site}&referred_module=sw&period=&begin_date=2010-01-01&end_date=2019-10-07"
 done
