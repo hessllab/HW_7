48b4e5fcd60e8b139aa5dc03ebfd0f4bf390ba18
zgrzywacz <54342413+zgrzywacz@users.noreply.github.com>
2019-10-06 22:26:37 -0400

    Collects flow data from USGS website

---
+++ flow.sh
@@ -0,0 +1,6 @@
+# Collect flow from 2010 to present
+# Usage: bash flow.sh date (year-month-day)
+for site in "02109500" "02134500" "02091814" "02105769"
+do
+ wget -O "$site".txt "https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$site"&period=&begin_date=2010-01-01&end_date="$1""
+done
