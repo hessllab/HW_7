2a9100741aac53f56dc8f871b6ffd990df7c2262
Tobi Ore <tmo0005@mix.wvu.edu>
2019-10-08 11:46:25 -0400

    I added a line of code that saves the most recent stream guage reading to a textfile called "current_flow.txt"

--- flow.sh
+++ flow.sh
@@ -3,4 +3,5 @@
 for site in "02109500" "02134500" "02091814" "02105769"
 do
  wget -O "$site".txt "https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$site"&period=&begin_date=2010-01-01&end_date="$1""
+ tail -1 "$site".txt >> current_flow.txt
 done
